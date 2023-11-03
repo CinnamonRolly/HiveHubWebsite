5b8212cfcf7c37642efbb603b513447ee4fb62b8
M <MungkornHorpanich@gmail.com>
2023-11-03 23:05:24 +0700

    fixing the footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='py-4 w-full text-white font-bold text-[14px] mx-auto'>
-      <h1 className='justify-center text-center'>© 2023 Hive Hub. All Rights Reserved.</h1>    
+      <h1 className='justify-center text-center'>© 2023 Hive Hub LLC. All Rights Reserved.</h1>    
     </div>
   )
 }
